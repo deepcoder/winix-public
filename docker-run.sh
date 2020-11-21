9ced33d79ab2e342deeab0a37b6b7e0087ac202f
Dave Proffer <dproffer@acm.org>
2020-11-21 01:00:45 -0800

    Docker run example for winix-02.py

--- docker-run.sh
+++ docker-run.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash 
 #
 # docker-run.sh
-# 202011051733  
+# 202011202303  
 #
 # http://redsymbol.net/articles/unofficial-bash-strict-mode/
 # Unofficial Bash Strict Mode (Unless You Looove Debugging)
 set -euo pipefail
 
-docker run -i -t -d --init --name="winix-01" \
+docker run -i -t -d --init --name="winix-02" \
     -v /home/user/winix:/home/user/winix \
     -v /etc/localtime:/etc/localtime:ro \
     --net=host \
     --user $(id -u):$(id -g) \
-    winix-01
+    winix-02
     
